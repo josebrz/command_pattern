792fc25280ccc28bb2e19fd68f80eb95cc8dec79
Jose Brizuela <jose.brizuela@modo.com.ar>
2022-06-26 23:39:00 -0300

    fishedddddddd

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import config from './config'
 import {router as ordenRouter} from "./football_dt/routes";
+import path from "path";
 
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname,"..",'views'));
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.render("pages/home")
+})
+
 app.use(config.ordenUrl, ordenRouter)
 
 app.listen(config.port, () => {
